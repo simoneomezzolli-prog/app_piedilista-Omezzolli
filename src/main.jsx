6a0e835b6e9a3446c0abe1cf2c504bf44dbecad6
simoneomezzolli-prog <simone.omezzolli@gmail.com>
2025-09-27 11:52:55 +0200

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import AppPiedilistaOmezzolli from './AppPiedilistaOmezzolli.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppPiedilistaOmezzolli />
+  </React.StrictMode>,
+)
